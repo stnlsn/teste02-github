3cc42dc79447df15d3ef6f6c6a0ffc9f72e9b5c6
stnlsn <nnsaito@uol.com.br>
2019-04-17 21:42:06 -0300

    Projeto criado

---
+++ src/input01/Prog01.java
@@ -0,0 +1,43 @@
+package input01;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Prog01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		// 35
+		int n1 = sc.nextInt();
+		// Bob Brown
+		sc.nextLine();
+		String name = sc.nextLine();
+		// F
+		char gender = sc.next().charAt(0);
+		// b5
+		String s = sc.next();
+		char letter = s.charAt(0);
+		int digit = Integer.parseInt(s.substring(1));
+		// 4.32
+		double n2 = sc.nextDouble();
+		// Maria F 23 1.68
+		String name2 = sc.next();
+		char ch = sc.next().charAt(0);
+		int age = sc.nextInt();
+		double height = sc.nextDouble();
+		System.out.println(n1);
+		System.out.println(name);
+		System.out.println(gender);
+		System.out.println(letter);
+		System.out.println(digit);
+		System.out.println(n2);
+		System.out.println(name2);
+		System.out.println(ch);
+		System.out.println(age);
+		System.out.println(height);
+		sc.close();
+	}
+
+}
